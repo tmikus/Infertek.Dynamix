739a5266504a9775414f8450af2302e6a2f8cb2a
Tomasz Mikus <klusi0@gmail.com>
2011-10-29 16:49:18 +0200

    Reorganized repository.

--- ClientSide/build_release.bat
+++ ClientSide/build_release.bat
@@ -1,11 +1,11 @@
 echo "Building version 0.1"
 echo @off
 rem Compiling version 0.1 of Infertek.Core
-"..\Tools\MergeFiles\MergeFiles\bin\Release\MergeFiles.exe" "Scripts\Common\DomExtensions.js" "Scripts\Common\Namespaces.js" "Scripts\Common\ScriptManager.js" -o "Scripts\Infertek.Core-0.1-debug.js"
-java -jar "YuiCompressor\yuicompressor-2.4.6.jar" "Scripts\Infertek.Core-0.1-debug.js" -o "Scripts\Infertek.Core-0.1.js"
+"..\Tools\MergeFiles\MergeFiles\bin\Release\MergeFiles.exe" "Source\Common\DomExtensions.js" "Source\Common\Namespaces.js" "Source\Common\ScriptManager.js" -o "Build\Infertek.Core-0.1-debug.js"
+java -jar "YuiCompressor\yuicompressor-2.4.6.jar" "Build\Infertek.Core-0.1-debug.js" -o "Build\Infertek.Core-0.1.js"
 
 rem Compiling version 0.1 of Infertek.Animations
-"..\Tools\MergeFiles\MergeFiles\bin\Release\MergeFiles.exe" "Scripts\Common\Exceptions.js" "Scripts\Animations\PropertyValueAnimators.js"  "Scripts\Animations\FrameBlendingFunctions.js" "Scripts\Animations\AnimationKeyframe.js" "Scripts\Animations\AnimationProperty.js" "Scripts\Animations\Animation.js" -o "Scripts\Infertek.Animations-0.1-debug.js"
-java -jar "YuiCompressor\yuicompressor-2.4.6.jar" "Scripts\Infertek.Animations-0.1-debug.js" -o "Scripts\Infertek.Animations-0.1.js"
+"..\Tools\MergeFiles\MergeFiles\bin\Release\MergeFiles.exe" "Source\Common\Exceptions.js" "Source\Animations\PropertyValueAnimators.js"  "Source\Animations\FrameBlendingFunctions.js" "Source\Animations\AnimationKeyframe.js" "Source\Animations\AnimationProperty.js" "Source\Animations\Animation.js" -o "Build\Infertek.Animations-0.1-debug.js"
+java -jar "YuiCompressor\yuicompressor-2.4.6.jar" "Build\Infertek.Animations-0.1-debug.js" -o "Build\Infertek.Animations-0.1.js"
 echo @on
 echo "Version 0.1 built"
